5cf1785bef3526e8f9ad8a318d4715c8c33b23c8
Иван <ivankiseliov@rambler.ru>
2022-09-22 20:53:49 +0300

    dz8

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
+        int salary = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + salary;
+            System.out.println("Месяц "+ i + " сумма " + total);
         }
     }
 }
