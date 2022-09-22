f104bc90dbed6d862ff095e9d248a1bcb0c20e81
Иван <ivankiseliov@rambler.ru>
2022-09-22 21:19:42 +0300

    dz9

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int salary = 29000;
         int total = 0;
         for (int i = 1; i <= 12; i++) {
-            total = total + total/12;
+            total = total + total/100;
             total = total + salary;
             System.out.println("Месяц "+ i + " сумма " + total);
         }
